08a5c162f8b49d05340931188f3924da8f78e501
bxsec <bxsecx@163.com>
2017-11-10 17:38:05 +0800

    4.打印驱动详细信息

--- 4.2查看WDM驱动结构信息/4.2查看WDM驱动结构信息/HelloWDM.cpp
+++ 4.2查看WDM驱动结构信息/4.2查看WDM驱动结构信息/HelloWDM.cpp
@@ -153,7 +153,7 @@ NTSTATUS HelloWDMPnp(IN PDEVICE_OBJECT fdo,
 	NTSTATUS status = STATUS_SUCCESS;
 	PDEVICE_EXTENSION pdx = (PDEVICE_EXTENSION)fdo->DeviceExtension;
 	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(pIrp);
-	static NTSTATUS (*fcntab[])(PDEVICE_EXTENSION pdx, PIRP Irp) = 
+	static NTSTATUS(*fcntab[])(PDEVICE_EXTENSION pdx, PIRP Irp) =
 	{
 		DefaultPnpHandler,		// IRP_MN_START_DEVICE
 		DefaultPnpHandler,		// IRP_MN_QUERY_REMOVE_DEVICE
@@ -179,8 +179,50 @@ NTSTATUS HelloWDMPnp(IN PDEVICE_OBJECT fdo,
 		DefaultPnpHandler,		// IRP_MN_QUERY_BUS_INFORMATION
 		DefaultPnpHandler,		// IRP_MN_DEVICE_USAGE_NOTIFICATION
 		DefaultPnpHandler,		// IRP_MN_SURPRISE_REMOVAL
+	};
+
+	ULONG fcn = stack->MinorFunction;
+	if (fcn >= arraysize(fcntab))
+	{
+		status = DefaultPnpHandler(pdx, pIrp);
+		return status;
 	}
 
+#if DBG
+	static char* fcnname[] = 
+	{
+		"IRP_MN_START_DEVICE",
+		"IRP_MN_QUERY_REMOVE_DEVICE",
+		"IRP_MN_REMOVE_DEVICE",
+		"IRP_MN_CANCEL_REMOVE_DEVICE",
+		"IRP_MN_STOP_DEVICE",
+		"IRP_MN_QUERY_STOP_DEVICE",
+		"IRP_MN_CANCEL_STOP_DEVICE",
+		"IRP_MN_QUERY_DEVICE_RELATIONS",
+		"IRP_MN_QUERY_INTERFACE",
+		"IRP_MN_QUERY_CAPABILITIES",
+		"IRP_MN_QUERY_RESOURCES",
+		"IRP_MN_QUERY_RESOURCE_REQUIREMENTS",
+		"IRP_MN_QUERY_DEVICE_TEXT",
+		"IRP_MN_FILTER_RESOURCE_REQUIREMENTS",
+		"",
+		"IRP_MN_READ_CONFIG",
+		"IRP_MN_WRITE_CONFIG",
+		"IRP_MN_EJECT",
+		"IRP_MN_SET_LOCK",
+		"IRP_MN_QUERY_ID",
+		"IRP_MN_QUERY_PNP_DEVICE_STATE",
+		"IRP_MN_QUERY_BUS_INFORMATION",
+		"IRP_MN_DEVICE_USAGE_NOTIFICATION",
+		"IRP_MN_SURPRISE_REMOVAL"
+	};
+
+	KdPrint(("PNP Request (%s)\n", fcnname[fcn]));
+#endif
+
+	status = (*fcntab[fcn])(pdx, pIrp);
+	KdPrint(("Leave HelloWDMPnp"));
+	return status;
 }
 
 NTSTATUS HelloWDMDispatchRoutine(IN PDEVICE_OBJECT fdo,
